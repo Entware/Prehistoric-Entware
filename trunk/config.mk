7829ed02bcab7344ecc441ee6b8cf69322890156
Alexander Ryzhov <ryzhovau@gmail.com>
2012-03-07 10:07:56 +0000

    !Unfinished! Locales turned on. Fixup according last OpenWRT trunk (rev.30831) Still no LD_LIBRARY_PATH support.

---
+++ trunk/config.mk
@@ -0,0 +1,5 @@
+TOOLCHAIN_REVISION:=r3702
+FIRMWARE_REVISION:=r3702
+# Make copy of a clean Buildroot trunk tree to write patches later.
+# You need it only for adding new patches in ./packages
+BUILDROOT_BACKUP:=yes
