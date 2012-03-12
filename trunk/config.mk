0783242d6409b55242769ab0c6829e5a5ad285be
Alexander Ryzhov <ryzhovau@gmail.com>
2012-03-12 05:33:57 +0000

    buildroot: rpath fix, broken in r385. Fixup according 30872 OpenWRT rev.

--- trunk/config.mk
+++ trunk/config.mk
@@ -1,5 +1,5 @@
-TOOLCHAIN_REVISION:=r3702
+TOOLCHAIN_REVISION:=r3458
 FIRMWARE_REVISION:=r3702
-# Make copy of a clean Buildroot trunk tree to write patches later.
-# You need it only for adding new patches in ./packages
+# Make a copy of clean Buildroot trunk tree to write patches later.
+# You need it only for adding new packages
 BUILDROOT_BACKUP:=yes
