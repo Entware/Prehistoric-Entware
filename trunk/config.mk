68e3d8b6d4a7029f7e51135fb266eb3ec6faeec5
Oleg Shashkov <voidshah@gmail.com>
2012-03-20 07:44:48 +0000

    Locales turned on (no more question during compilation). Change path for dynamic linker to /opt/lib

--- trunk/config.mk
+++ trunk/config.mk
@@ -1,5 +1,5 @@
-TOOLCHAIN_REVISION:=r3458
-FIRMWARE_REVISION:=r3702
+TOOLCHAIN_REVISION:=r3988
+FIRMWARE_REVISION:=r4001
 # Make a copy of clean Buildroot trunk tree to write patches later.
 # You need it only for adding new packages
 BUILDROOT_BACKUP:=yes
