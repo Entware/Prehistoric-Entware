b102b96b4b82f81b626be556b660c223e6d66ac5
Alexander Ryzhov <ryzhovau@gmail.com>
2012-04-11 08:58:24 +0000

    Entware Buildroot scripts renewed, an OpenWRT/toolchain revisions now controlled via config.mk, basic libc libraries names renewed

--- trunk/config.mk
+++ trunk/config.mk
@@ -1,5 +1,7 @@
-TOOLCHAIN_REVISION:=r3988
-FIRMWARE_REVISION:=r4001
+# Please do not change revision until you know exactly what are you doing^)
+
+TOOLCHAIN_REVISION:=r4086
+OPENWRT_REVISION:=31158
 # Make a copy of clean Buildroot trunk tree to write patches later.
 # You need it only for adding new packages
 BUILDROOT_BACKUP:=yes
