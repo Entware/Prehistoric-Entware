67bff978317d390f8697e73c9b4efb0727b4508e
Alexander Ryzhov <ryzhovau@gmail.com>
2012-06-01 16:36:18 +0000

    Fixup according to last (r32017) OpenWRT trunk

--- trunk/config.mk
+++ trunk/config.mk
@@ -1,7 +1,7 @@
 # Please do not change revision until you know exactly what are you doing^)
 
 TOOLCHAIN_REVISION:=r4086
-OPENWRT_REVISION:=31549
+OPENWRT_REVISION:=32017
 # Make a copy of clean Buildroot trunk tree to write patches later.
 # You need it only for adding a new packages
 BUILDROOT_BACKUP:=yes
