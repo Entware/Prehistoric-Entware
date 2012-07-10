45ce970908500e409733cad4349b1bc78a3fdacc
Alexander Ryzhov <ryzhovau@gmail.com>
2012-07-10 12:48:47 +0000

    Toolchain: bumped to r4489. A new GCC 4.5.4.

--- trunk/config.mk
+++ trunk/config.mk
@@ -8,9 +8,9 @@
 # Nothing should ever be touched by mere mortals. Unless you hang out
 # with the gods, you should probably leave all this stuff alone.
 
-
-TOOLCHAIN_REVISION:=r4086
+TOOLCHAIN_REVISION:=r4489
 OPENWRT_REVISION:=32017
-# Make a copy of clean Buildroot trunk tree to write patches later.
-# You need it only for adding a new packages
+
+# Make a copy of clean OpenWRT trunk tree to write patches later.
+# You need it only for adding a new packages.
 BUILDROOT_BACKUP:=yes
