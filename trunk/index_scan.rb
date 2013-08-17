b5e3d0004e351973edaa7b9bb6e7a2ed2a0d20bb
Roman Samarev <rssdev10>
2013-08-17 12:54:07 +0000

    fix unified package processing

--- trunk/index_scan.rb
+++ trunk/index_scan.rb
@@ -2,8 +2,7 @@
 
 FILE_ENCODING = (RUBY_VERSION.include? '1.8') ? "\n" : {:encoding => 'iso-8859-1'}
 
-File.open '.packages_path.mk', 'w' do |fout|
-  fout.puts "# Automatically generated file. Yours sincerely, script #{__FILE__}. #{Time.now}.\n\n"
+result = {}
 
   Dir['../openwrt_trunk/feeds/*.index'].each do |fn|
     puts 'Processing: ' + fn
@@ -19,9 +18,18 @@ File.open '.packages_path.mk', 'w' do |fout|
         package_path = fpath + val[0..val.rindex('/Makefile')]
       when 'Package'
         package = val.downcase
-        fout.puts %Q(PK_NAME_#{package}="#{package_path}")
-      end
+        result[package] = package_path
 
+        # check if package name is not the directory name
+        unless package_path.include? "\/#{package}\/"
+          # add the directory name as package name
+          result[ package_path[package_path[0..-2].rindex('/')+1 .. -2]] = package_path
+        end
+      end
     end
   end
+
+File.open '.packages_path.mk', 'w' do |fout|
+  fout.puts "# Automatically generated file. Yours sincerely, script #{__FILE__}. #{Time.now}.\n\n"
+  result.sort_by{ |key,val| key }.each { |key, val| fout.puts %Q(PK_NAME_#{key}="#{val}") }
 end
