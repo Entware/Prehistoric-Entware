c973c5c0752e1a770215e25f168e7b8148f97082
Alexander Ryzhov <github@ryzhov-al.ru>
2016-04-02 07:54:25 +0300

    openwrt: switch from original svn to github git mirror

--- config.mk
+++ config.mk
@@ -11,8 +11,8 @@ export KERNEL_COMMIT=a449bfd2b99a2b19fa64c176d385b29432327a12
 # Commit taken from https://github.com/wl500g/toolchain
 export TOOLCHAIN_COMMIT=3a135e5c46204ebad56b54417b320e2b057bcdc1
 
-# See https://dev.openwrt.org/timeline
-export OPENWRT_REVISION=47008
+# See commit log at https://github.com/openwrt/openwrt/commits/master
+export OPENWRT_COMMIT=8bb1952983fc3fc15455ef3761fd2298cd86a941
 
 # Download or compile toolchain\kernel?
 #FORCE_COMPILE=y
